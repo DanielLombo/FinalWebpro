f2c5f0d2fe6e8b7c027ce48cb8e5ede9bc8b8677
DanielLombo <64979810+DanielLombo@users.noreply.github.com>
2020-12-08 03:19:53 +0800

    provinsi fix
    8-12-2020

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ const App = () => {
   return (
     <BrowserRouter>
       <div >
-        <nav><Navbar bg="light" expand="lg">
+        <nav><Navbar bg="danger" expand="lg">
           <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href=".">Global</Nav.Link>
+              <Nav.Link href="/indonesia">indonesia</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
--- src/provinsi.js
+++ src/provinsi.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Table } from 'react-bootstrap'
 
-const Provinsi =()=> {
+const Provinsi = () => {
   const [prov, setProv] = useState([]);
 
   useEffect(() => {
@@ -15,9 +16,34 @@ const Provinsi =()=> {
 
   return (
     <div>
+
       provinsi
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          {prov.map((items)=>{return(
+            <tr>
+            <td>{items.provinsi}</td>
+          <td>{items.kasusPosi}</td>
+          <td>{items.kasusSemb}</td>
+          <td>{items.kasusMeni}</td>
+          </tr>
+          )
+          }) }
+          
+        </tbody>
+      </Table>
     </div>
   );
 }
 
 export default Provinsi;
+
+
