fbb3896962d62a2972689eb47c3b504ce3b6631f
DanielLombo <64979810+DanielLombo@users.noreply.github.com>
2020-12-08 01:08:50 +0800

    commit
    8-12-2020

---
+++ src/indo.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function indo() {
+  return (
+    <div>
+      indo
+    </div>
+  );
+}
+
+export default indo;
---
+++ src/provinsi.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function provinsi() {
+  return (
+    <div>
+      provinsi
+    </div>
+  );
+}
+
+export default provinsi;
