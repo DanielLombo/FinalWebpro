b83cf1d2fc131daf1c9760736278d81792ec6d2c
DanielLombo <64979810+DanielLombo@users.noreply.github.com>
2020-12-08 02:46:31 +0800

    Update Navbar
    8-12-2020

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
-import {BrowserRouter, Switch, Link, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
 import './App.css';
 import global from "./global";
 import indo from "./indo";
 import provinsi from "./provinsi";
+import {Navbar,  NavDropdown, Form, Nav, FormControl, Button} from 'react-bootstrap';
 
-const App =()=> {
+const App = () => {
   return (
     <BrowserRouter>
-    <div >
-     <nav>
-       <ul>
-       <li>
-         <Link to='/'>Global</Link>
-         </li>
-         <li>
-         <Link to='/indonesia'>Indonesia</Link>
-         </li>
-         <li>
-         <Link to='/provinsi'>Provinsi</Link>
-         </li>
-       </ul>
-     </nav>
-     <Switch>
-       <Route exact path='/' component={global}/>
-       <Route path='/indonesia' component={indo}/>
-       <Route path='/provinsi' component={provinsi}/>
-     </Switch>
-    </div>
+      <div >
+        <nav><Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+          <ul>
+            <li>
+              <Link to='/'>Global</Link>
+            </li>
+            <li>
+              <Link to='/indonesia'>Indonesia</Link>
+            </li>
+            <li>
+              <Link to='/provinsi'>Provinsi</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={global} />
+          <Route path='/indonesia' component={indo} />
+          <Route path='/provinsi' component={provinsi} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
--- src/provinsi.js
+++ src/provinsi.js
@@ -1,6 +1,18 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Provinsi =()=> {
+  const [prov, setProv] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://indonesia-covid-19.mathdro.id/api/provinsi")
+      .then((res) => {
+        setProv(res.data.data);
+      });
+  }, []);
+
   return (
     <div>
       provinsi
